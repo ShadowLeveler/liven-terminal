f87853b73f31224aa4f43021b5ed0e105bc41536
ShadowLeveler <ali1921haider@gmail.com>
2025-07-02 11:14:36 -0700

    main.py

--- main.py
+++ main.py
@@ -40,7 +40,26 @@ if not st.session_state["credentials"]:
             "https://www.googleapis.com/auth/spreadsheets",
             "https://www.googleapis.com/auth/drive.file",
         ],
-        redirect_uri=REDIRECT_URI
+        REDIRECT_URI = "https://liven-terminal-yjkmpuuqdzfq3lep9h4c7d.streamlit.app"
+
+flow = Flow.from_client_config(
+    {
+        "web": {
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "redirect_uris": [REDIRECT_URI],
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token"
+        }
+    },
+    scopes=[
+        "https://www.googleapis.com/auth/documents",
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive.file",
+    ],
+    redirect_uri=REDIRECT_URI
+)
+
     )
 
     auth_url, _ = flow.authorization_url(prompt='consent')
